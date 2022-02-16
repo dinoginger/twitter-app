9e9a1342a0f47cdb204b303a99d7ae5700275512
marco <marcothemars@gmail.com>
2022-02-16 12:48:57 +0200

    made a redirect just for fun ig

--- 3/main.py
+++ 3/main.py
@@ -7,7 +7,7 @@ and Leaflet.js to map given users' locations on map.
 """
 import fastapi
 import jinja2
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -19,15 +19,16 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def something():
-    return {"something"}
+    response = RedirectResponse("/input")
+    return response
 
 
-@app.get('/index/{name}', response_class=HTMLResponse)
+@app.get('/input', response_class=HTMLResponse)
 async def read_item(request: fastapi.Request):
     return templates.TemplateResponse('index.html', {'request': request})
 
 
-@app.post("/index/{name}")
+@app.post("/input")
 async def get_item(request: fastapi.Request, username: str = fastapi.Form(...)):
     # return templates.TemplateResponse('index.html', {'request': request, 'name': username})
     print({'request': request, 'name': username})
